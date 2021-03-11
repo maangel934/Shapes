a6b0cacddf6979540276625f47b78787f24d0ca0
Miguel Angel <marodriguezd@iesch.org>
2021-03-11 17:03:20 +0100

    Añadimos documentacion a la interfaz IArea

--- Shapes/IArea.cs
+++ Shapes/IArea.cs
@@ -1,5 +1,8 @@
 ﻿namespace Shapes
 {
+    /// <summary>
+    /// interfaz que contiene los valores del área del un círculo
+    /// </summary>
     public interface IArea
     {
         double Area { get; }
