a67309822cbe76150ca73eb2887b8ec8781a5f80
Miguel Angel <marodriguezd@iesch.org>
2021-03-11 17:44:58 +0100

    Creamos el nuevo archivo en el cual realizaremos las pruebas

---
+++ PruebasRectangle/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shapes;
+namespace PruebasRectangle
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
